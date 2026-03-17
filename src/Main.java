f9e78262d6366383980126f913c8f8f81e6c8e74
DerMaster <bjzeysing@gmx.de>
2026-03-17 15:25:56 +0100

    + Restructur

--- src/Main.java
+++ src/Main.java
@@ -7,40 +7,20 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        int port = 12345;
-
-        DatagramSocket socket = new DatagramSocket(null);
-        socket.bind(new InetSocketAddress("::", port));
-
-        System.out.println("UDP Server listening to Port " + port);
 
         SQL sql = new SQL("sensor_user", "sensor_pass");
 
-        byte[] buffer = new byte[1024];
+        System.out.println();
+
+        UDP udp = new UDP(12345);
 
         Scanner scanner = new Scanner(System.in);
+        while (!scanner.hasNext()) {}
 
-        while (!scanner.hasNext()) {
-            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-            socket.receive(packet);
-
-            String payload = new String(
-                    packet.getData(),
-                    packet.getOffset(),
-                    packet.getLength(),
-                    StandardCharsets.UTF_8
-            );
-
-            String from = packet.getAddress().getHostAddress();
-
-            System.out.println("From " + from + ": " + payload);
-        }
-        System.out.println("Scanner reading: "+scanner.next());
         System.out.println("Stopping Services");
         sql.close();
-        System.out.println("Stopping UDP Server");
-        socket.close();
-        System.out.println("UDP Server stopped");
+        System.out.println();
+        udp.close();
     }
 }
 
---
+++ src/UDP.java
@@ -0,0 +1,59 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
+import java.net.SocketException;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+public class UDP extends Thread {
+
+    int port;
+    DatagramSocket socket;
+
+    public UDP(int port) throws SocketException {
+        this.port = port;
+        startServer();
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        System.out.println("Started Thread");
+        byte[] buffer = new byte[1024];
+        while (!socket.isClosed()){
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+            try {
+                socket.receive(packet);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            String payload = new String(
+                    packet.getData(),
+                    packet.getOffset(),
+                    packet.getLength(),
+                    StandardCharsets.UTF_8
+            );
+
+            String from = packet.getAddress().getHostAddress();
+
+            System.out.println("From " + from + ": " + payload);
+        }
+        System.out.println("Stopped Thread");
+    }
+
+    public void startServer() throws SocketException {
+        System.out.println("Starting Server on "+port);
+        socket = new DatagramSocket(null);
+        socket.bind(new InetSocketAddress("::", port));
+        System.out.println("UDP Server listening to Port " + port);
+    }
+
+    public void close(){
+        System.out.println("Stopping UDP Server");
+        socket.close();
+        System.out.println("UDP Server stopped");
+    }
+
+}
